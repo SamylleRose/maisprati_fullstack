4ab0b52329b95d40d50859dee75c97b6df1b844a
SamylleRose <samylle1brito@gmail.com>
2024-04-20 21:02:33 -0300

    primeiro commit

---
+++  exercise/list01/01_temperature.js
@@ -0,0 +1,6 @@
+let temperatureCelsius = prompt("Digite uma temperatura em celsius: ");
+temperatureCelsius = parseInt(temperatureCelsius);
+
+const temperatureFahrenheit = temperatureCelsius * 1.8 + 32;
+
+console.log(temperatureFahrenheit);
---
+++  exercise/list01/03_four_numbers.js
@@ -0,0 +1,14 @@
+let numberOne = parseInt(prompt("The Number One "));
+let numberTwo = parseInt(prompt(" The Number Two"));
+let numberThree = parseInt(prompt("The Number Three"));
+let numberFour = parseInt(prompt("The Number Four"));
+
+numberFour = numberOne + numberTwo + numberThree;
+numberThree = (12 * numberThree) / 100;
+numberOne += 25;
+numberTwo *= 3;
+
+console.log(numberOne);
+console.log(numberTwo);
+console.log(numberThree);
+console.log(numberFour);
